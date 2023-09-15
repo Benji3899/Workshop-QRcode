a4e75e50c6b5c64dce07b9a22bb9ee7cafa16aef
Benjamin EPS <benjamin_eps@hotmail.fr>
2023-09-15 12:17:55 +0200

    correct conflit

--- src/Controller/QrCodeController.php
+++ src/Controller/QrCodeController.php
@@ -10,21 +10,9 @@ use Twig\Environment;
 class QrCodeController extends AbstractController
 {
     #[Route('/qrcode', name: 'app_qr_code')]
-//    public function index(): Response
-//    {
-//        $user = $this->getUser();
-//        return new Response(sprintf('Hello %s', $user->getNom()));
-//
-//        return $this->render('qr_code/qr-code.html.twig', [
-//            'controller_name' => 'QrCodeController',
-//        ]);
-//    }
 
     public function index(Environment $twig): Response
     {
-
-        return $this->render('qr_code/qr-code.html.twig', ['controller_name' => 'QrCodeController',]);
-
         $user = $this->getUser();
         return new Response($twig->render('qr_code/qr-code.html.twig', [
             'nom' => $user->getNom(),
